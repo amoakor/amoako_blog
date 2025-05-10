82ac3dee9e0c7298d7eccbfb3b1876d6346975b4
amoakor <richard.damoako@gmail.com>
2025-05-09 22:12:45 -0400

    Add files via upload

---
+++ blogscript.R
@@ -0,0 +1,20 @@
+<iframe src="path/to/publications_map.html" width="100%" height="500px"></iframe>
+  
+  ![](images/community%20network.jpg){width="436"}
+
+page-footer: "@RD Amoako  Powered by Quarto"analytics:
+  google:
+  tracking-id: "G-C0M53F4HT8"
+
+{{< include _footer.qmd >}}
+
+footer:
+  left: "© 2025 RD Amoako"
+right: "Powered by <a href='https://quarto.org/'>Quarto</a>"
+
+
+```{r}
+#| echo: false
+#| out-width: 100%
+htmltools::includeHTML("assets/publications_map.html")
+```
